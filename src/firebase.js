b4a614a5a32586c77c24f0acc4c17a69662fc618
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-05 00:10:28 +0300

    Update Firebase configuration and API keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Veritabanı için bunu ekledik
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBr4wfYpRSdeR1JBDzmFwWq_dBmfy5rvnM",
+  apiKey: "AIzaSyCx...", // Senin kendi apiKey'in kalsın
   authDomain: "angry-neigbour-2.firebaseapp.com",
   projectId: "angry-neigbour-2",
   storageBucket: "angry-neigbour-2.firebasestorage.app",
-  messagingSenderId: "218919463759",
-  appId: "1:218919463759:web:f509cfbaa87fccd1afb33a"
+  messagingSenderId: "1086259024095",
+  appId: "1:1086259024095:web:8e309..."
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Veritabanını dışa aktar ki App.js içinde kullanabilelim
 export const db = getFirestore(app);
-
